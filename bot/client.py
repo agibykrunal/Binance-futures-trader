f6e8413c9d90f8d552b9cea781cf193d02d1a091
Krunal <nsundersia@gmail.com>
2026-04-18 18:41:47 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,6 @@
 """
 Binance Futures Testnet REST API client.
-dhhdhdhfhhfhfhhhdhshhhhahahahhah
+dhhdhdhfhhfhfhhhdhshhhhahahahha
 Wraps raw HTTP interactions: signing, timestamp injection,
 request/response logging, and structured error handling.
 """
