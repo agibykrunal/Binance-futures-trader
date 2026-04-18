65a04e2669af64d0ae837bf2048a2f32a338998d
Krunal <nsundersia@gmail.com>
2026-04-18 18:44:07 +0530

    Fix typo in client.py documentation

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,6 @@
 """
 Binance Futures Testnet REST API client.
-dhhdhdhfhhfhfhhhdh
+dhhdhdhfhhfhfhhhd
 Wraps raw HTTP interactions: signing, timestamp injection,
 request/response logging, and structured error handling.
 """
